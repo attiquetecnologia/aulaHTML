da4f3139ac7343a883c2aa956b8330f0c711b932
attiquetecnologia <rodrigoatique@gmail.com>
2024-09-19 10:45:37 -0300

    recebendo valores

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask # Importa o flask
+from flask import (Flask, request) # Importa o flask
 
 app = Flask(__name__) # cria uma instância
 
 @app.route("/", methods=('GET',)) # Assina uma rota
 def index(): # função responsável pela página
-    nome = 'Rodrigo' # use o seu nome
+    nome = request.args.get('nome') # use o seu nome
     # HTML retornado
     return f"""<h1>Página inicial</h1>
         <p>Olá {nome}, que nome bonito!
